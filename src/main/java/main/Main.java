38d76753fdfca91dcbc8424bc83b9ff3a748b7a4
Danielit707 <ceradaniel517@gmail.com>
2024-04-21 23:19:06 -0500

    packages

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.segundo_parcial;
+package main;
 
 /**
  *
  * @author User
  */
-public class Segundo_parcial {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
