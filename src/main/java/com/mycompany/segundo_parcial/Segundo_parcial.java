593458ae80435012f7ee90f18ccfaa3b92e27f3b
Danielit707 <ceradaniel517@gmail.com>
2024-04-21 21:57:01 -0500

    first commit

---
+++ src/main/java/com/mycompany/segundo_parcial/Segundo_parcial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.segundo_parcial;
+
+/**
+ *
+ * @author User
+ */
+public class Segundo_parcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
